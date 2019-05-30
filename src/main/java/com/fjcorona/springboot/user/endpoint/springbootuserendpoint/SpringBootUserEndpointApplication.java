4fe9ae955c2f451c1a9a2bab2bd51ae23e561d28
Javo Crown <fjavier.corona@itexico.net>
2019-05-30 10:03:01 -0500

    Structure

---
+++ src/main/java/com/fjcorona/springboot/user/endpoint/springbootuserendpoint/SpringBootUserEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.fjcorona.springboot.user.endpoint.springbootuserendpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUserEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUserEndpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fjcorona/springboot/user/endpoint/springbootuserendpoint/SpringBootUserEndpointApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fjcorona.springboot.user.endpoint.springbootuserendpoint;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootUserEndpointApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
