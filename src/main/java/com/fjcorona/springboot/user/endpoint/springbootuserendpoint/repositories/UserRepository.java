bc7803327d115ac9ee0074f8bd420dc2e2200179
Javo Crown <fjavier.corona@itexico.net>
2019-05-30 14:15:01 -0500

    Repository, change Integer instead Long

--- src/main/java/com/fjcorona/springboot/user/endpoint/springbootuserendpoint/repositories/UserRepository.java
+++ src/main/java/com/fjcorona/springboot/user/endpoint/springbootuserendpoint/repositories/UserRepository.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.fjcorona.springboot.user.endpoint.springbootuserendpoint.models.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 	List<User> findAll();
 	Optional<User> findById(Integer id);
 }
