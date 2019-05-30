10394e15c585b325682286bc49639ddfa9301abc
Javo Crown <fjavier.corona@itexico.net>
2019-05-30 11:19:46 -0500

    Add rest controller performance

--- src/main/java/com/fjcorona/springboot/user/endpoint/springbootuserendpoint/controllers/UserController.java
+++ src/main/java/com/fjcorona/springboot/user/endpoint/springbootuserendpoint/controllers/UserController.java
@@ -1,5 +1,23 @@
 package com.fjcorona.springboot.user.endpoint.springbootuserendpoint.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fjcorona.springboot.user.endpoint.springbootuserendpoint.models.User;
+import com.fjcorona.springboot.user.endpoint.springbootuserendpoint.repositories.UserRepository;
+
+@RestController
 public class UserController {
 
+	@Autowired
+	private UserRepository repository;
+
+	@GetMapping("/users")
+	public List<User> retriveAllUsers() {
+		List<User> users = repository.findAll();
+		return users;
+	}
 }
