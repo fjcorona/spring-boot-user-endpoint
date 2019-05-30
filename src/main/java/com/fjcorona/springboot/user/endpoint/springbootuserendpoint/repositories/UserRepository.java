1fb01571a8bca0b865ca65bf257603aa4ff3b817
Javo Crown <fjavier.corona@itexico.net>
2019-05-30 11:00:53 -0500

    Add JPA Repository to User

---
+++ src/main/java/com/fjcorona/springboot/user/endpoint/springbootuserendpoint/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.fjcorona.springboot.user.endpoint.springbootuserendpoint.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.fjcorona.springboot.user.endpoint.springbootuserendpoint.models.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	List<User> findAll();
+}
