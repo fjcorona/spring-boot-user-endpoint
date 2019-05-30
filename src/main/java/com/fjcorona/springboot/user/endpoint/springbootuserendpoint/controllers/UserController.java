d7957a052c07f079871c63116d1dab4fe6c34f42
Javo Crown <fjavier.corona@itexico.net>
2019-05-30 15:18:27 -0500

    Update user

--- src/main/java/com/fjcorona/springboot/user/endpoint/springbootuserendpoint/controllers/UserController.java
+++ src/main/java/com/fjcorona/springboot/user/endpoint/springbootuserendpoint/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -38,13 +39,29 @@ public class UserController {
 	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		user.setActive(1);
 		User savedUser = repository.save(user);
-		
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
 				.path("/{id}")
 				.buildAndExpand(savedUser.getId())
 				.toUri();
-		
+
 		return ResponseEntity.created(location).build();
+	}
+
+	@PutMapping("/users/{id}")
+	public ResponseEntity<Object> updateUser(@RequestBody User user, @PathVariable Integer id) {
+		Optional<User> userOptional = repository.findById(id);
+
+		if (!userOptional.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+
+		user.setId(id);
+
+		repository.save(user);
+
+		return ResponseEntity.noContent().build();
 	}
 
 }
