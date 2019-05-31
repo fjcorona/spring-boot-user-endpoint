a29a2c7aa8214e1de0585ce7d23e1f03743accea
Javo Crown <fjavier.corona@itexico.net>
2019-05-31 12:03:48 -0500

    get active users

--- src/main/java/com/fjcorona/springboot/user/endpoint/springboot/user/endpoint/repositories/UserRepository.java
+++ src/main/java/com/fjcorona/springboot/user/endpoint/springboot/user/endpoint/repositories/UserRepository.java
@@ -8,6 +8,8 @@ import com.fjcorona.springboot.user.endpoint.springboot.user.endpoint.models.Use
 public interface UserRepository extends JpaRepository<User, Integer> {
 	List<User> findAll();
 	Optional<User> findById(Integer id);
+	public List<User> findByActive(Integer active);
+	Optional<User> findByActiveAndId(Integer active, Integer id);
 	@SuppressWarnings("unchecked")
 	User save(User user);
 	void deleteById(Integer id);
