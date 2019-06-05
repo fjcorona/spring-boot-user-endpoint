3abc214c2277f5abab64053d0522231a3d4c5e80
Javo Crown <fjavier.corona@itexico.net>
2019-06-05 11:40:32 -0500

    Update UserRepository.java

--- src/main/java/com/fjcorona/springboot/user/endpoint/repositories/UserRepository.java
+++ src/main/java/com/fjcorona/springboot/user/endpoint/repositories/UserRepository.java
@@ -12,16 +12,16 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	// Retrieve on "User" by id, in case is found
 	Optional<User> findById(Integer id);
 	
-	// Retrive the list of active users: findByActive(1), or inactive: findByActive(0)
-	public List<User> findByActive(Integer active);
-	
-	// Retrieve one Active User by Id findByActiveAndId(1, :id)
-	Optional<User> findByActiveAndId(Integer active, Integer id);
-	
 	// Add/Update a User entity
 	@SuppressWarnings("unchecked")
 	User save(User user);
 	
 	// Delete user by Id
 	void deleteById(Integer id);
+	
+	// Retrive the list of active users: findByActive(1), or inactive: findByActive(0)
+	public List<User> findByActive(Integer active);
+	
+	// Retrieve one Active User by Id findByActiveAndId(1, :id)
+	Optional<User> findByActiveAndId(Integer active, Integer id);
 }
